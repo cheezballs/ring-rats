553690c0b29007ae4490205c0d3351f07749e7cb
cheezballs <sweetcrapmatt@gmail.com>
2023-12-01 12:52:01 -0600

    Java baby!

---
+++ src/main/java/dev/indoors/ringrats/RingRats.java
@@ -0,0 +1,12 @@
+package dev.indoors.ringrats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingRats {
+    public static void main(String[] args) {
+        SpringApplication.run(RingRats.class, args);
+    }
+}
+
