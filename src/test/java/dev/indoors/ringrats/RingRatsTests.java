5c2ff3afa5b43b6a84b08c4c3500d6fe093c6c38
cheezballs <sweetcrapmatt@gmail.com>
2023-12-01 17:26:39 -0600

    More stuff.

--- src/test/java/dev/indoors/ringrats/RingRatsTests.java
+++ src/test/java/dev/indoors/ringrats/RingRatsTests.java
@@ -3,10 +3,14 @@ package dev.indoors.ringrats;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class RingRatsTests {
+
     @Test
     void contextLoads() {
+        assertThat(1).isEqualTo(1);
     }
 }
 
