ca6982a141b26b3f1c2dd0d1ce529868913aefbd
cheezballs <sweetcrapmatt@gmail.com>
2023-12-01 13:04:42 -0600

    Java baby!

---
+++ src/test/java/dev/indoors/ringrats/RingRatsTests.java
@@ -0,0 +1,15 @@
+package dev.indoors.ringrats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RingRatsTests {
+    @Test
+    void contextLoads() {
+    }
+}
+
+
+
+
